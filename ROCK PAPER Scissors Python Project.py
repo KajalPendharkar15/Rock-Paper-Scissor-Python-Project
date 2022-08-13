7db736f484151a1a85c06952f172b289941b1e75
KajalPendharkar15 <111169298+KajalPendharkar15@users.noreply.github.com>
2022-08-13 13:05:13 +0530

    Add files via upload

---
+++ ROCK PAPER Scissors Python Project.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ROCK PAPER SCISSORS Python Project
+# 
+
+# In[1]:
+
+
+print('Rock...')
+print('Paper...')
+print('Scissors...')
+
+
+# In[5]:
+
+
+player1 = input("Player 1, make your move:")
+player2 = input("Player 2, make your move:")
+
+
+# In[9]:
+
+
+if player1 == 'Rock' and player2 == 'Scissors':
+    print("Player1 wins")
+elif player1 == 'Rock' and player2 == 'paper':
+      print("Player2 wins")
+elif player1 == 'paper' and player2 == 'Rock': 
+      print("Player1 wins")
+elif player1 == 'paper' and player2 == 'Scissors': 
+     print("Player2 wins")
+elif player1 == 'Scissors' and player2 == 'Rock': 
+    print("Player2 wins")
+elif player1 == 'Scissors' and player2 == 'paper':
+          print("Player1 wins")
+elif player1 == player2:
+    print('OOps its a tie...!')
+else: 
+    print('Something wennt wrong!')
+
+
+# # NESTED IF....ELSE STATEMENT
+# 
+
+# In[10]:
+
+
+if player1 == player2:
+    print("Its a tie!")
+elif player1 == 'Rock':
+    if player2 == 'Scissors':
+            print("Player1 wins")
+    elif player2 == 'paper':
+         print("Player2 wins")
+elif player1 == 'paper':
+        if player2 == 'Rock':
+             print("Player1 wins")
+        elif player2 == 'Scissors':
+            print("Player2 wins")
+elif player1 == 'Scissors':   
+     if player2 == 'paper':
+            print("Player1 wins")
+     elif player2 == 'Rock':
+        print("Player2 wins")
+else: 
+    print('Something wennt wrong!')
+  
+            
+
+        
+    
+
+
+# In[ ]:
+
+
+
+
